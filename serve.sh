36c8f19a1aa0821af29131251783a3a99e426782
JonG <jongarrison@gmail.com>
2025-11-04 23:59:49 -0800

    Dir change

--- serve.sh
+++ serve.sh
@@ -6,8 +6,8 @@
 PORT=${1:-8000}
 
 echo "Starting web server on http://localhost:$PORT"
-echo "Serving files from ./public/"
+echo "Serving files from ./docs/"
 echo "Press Ctrl+C to stop the server"
 echo ""
 
-cd public && python3 -m http.server $PORT
+cd docs && python3 -m http.server $PORT
